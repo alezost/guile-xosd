8ece6bd897cde7f0165d7fa1fe9d285df21934b7
Alex Kost <alezost@gmail.com>
2017-01-09 22:35:32 +0300

    guix: Remove the package for the latest version
    Since it is a part of Guix.
    
    * guix.scm (guile-xosd): Remove.

--- guix.scm
+++ guix.scm
@@ -1,6 +1,6 @@
 ;;; guix.scm --- Guix package for Guile-XOSD
 
-;; Copyright © 2016 Alex Kost <alezost@gmail.com>
+;; Copyright © 2016–2017 Alex Kost <alezost@gmail.com>
 
 ;; This file is part of Guile-XOSD.
 
@@ -19,15 +19,11 @@
 
 ;;; Commentary:
 
-;; This file contains 2 Guix packages for Guile-XOSD: for the latest
-;; release and for the development snapshot (i.e., for the current
-;; commit of the git checkout).  To build or install, run:
+;; This file contains Guix package for development version of
+;; Guile-XOSD.  To build or install, run:
 ;;
 ;;   guix build --file=guix.scm
 ;;   guix package --install-from-file=guix.scm
-;;
-;; (this will build/install the development package using the current
-;; git checkout directory).
 
 ;; Also you can use this file to make a development environment for
 ;; building Guile-XOSD:
@@ -47,56 +43,15 @@
  (srfi srfi-26)
  (guix gexp)
  (guix packages)
- (guix download)
- (guix git-download)
- (guix licenses)
  (guix build utils)
- (guix build-system gnu)
  (gnu packages autotools)
  (gnu packages guile)
- (gnu packages pkg-config)
- (gnu packages texinfo)
- (gnu packages xdisorg)
- (gnu packages xorg))
-
-(define %source-dir (dirname (current-filename)))
-
-
-;;; Package for the latest release
-
-(define guile-xosd
-  (package
-    (name "guile-xosd")
-    (version "0.2.1")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://github.com/alezost/" name
-                                  "/releases/download/v" version
-                                  "/" name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "1ri5065c16kmgrf2pysn2ymxjqi5302lhpb07wkl1jr75ym8fn8p"))))
-    (build-system gnu-build-system)
-    (native-inputs
-     `(("pkg-config" ,pkg-config)))
-    (inputs
-     `(("guile" ,guile-2.0)
-       ("libx11" ,libx11)
-       ("libxext" ,libxext)
-       ("libxinerama" ,libxinerama)
-       ("xosd" ,xosd)))
-    (home-page "https://github.com/alezost/guile-xosd")
-    (synopsis "XOSD bindings for Guile")
-    (description
-     "Guile-XOSD provides Guile bindings for @code{libxosd}, the \"X On
-Screen Display\" library.")
-    (license gpl3+)))
-
-
-;;; Git checkout and development package
+ (gnu packages texinfo))
 
 ;; The code for finding git files is based on
 ;; <https://git.dthompson.us/guile-sdl2.git/blob/HEAD:/guix.scm>.
+
+(define %source-dir (dirname (current-filename)))
 
 (define (git-output . args)
   "Execute 'git ARGS ...' command and return its output without trailing
