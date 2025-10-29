89e2ef8463471df278c1828ac3be35a3cae7e682
Alex Kost <alezost@gmail.com>
2025-10-29 11:35:35 +0300

    Replace auxiliary C library with Guile code
    At first, this significantly simplifies the package, as we get rid of
    the additional C code completely.  Also this auxiliary library uses
    SMOBs (small objects) which can be deprecated in future.
    See (info "(guile) Smobs") for details.
    
    * src/Makefile.am: Remove file.
    * src/libguile-xosd.c: Remove file.
    * configure.ac: Adjust accordingly.
    * Makefile.am: Likewise.
    * modules/Makefile.am: Likewise.
    * modules/xosd/config.scm.in: Likewise.
    * pre-inst-env.el.in: Likewise.
    * pre-inst-env.in: Likewise.
    * .gitignore: Likewise.
    * modules/xosd/bindings.scm: Use Guile foreign object interface to link
      "libxosd" library its procedures.

--- modules/xosd/bindings.scm
+++ modules/xosd/bindings.scm
@@ -1,6 +1,6 @@
 ;;; bindings.scm --- Guile bindings for libxosd
 
-;; Copyright © 2016 Alex Kost <alezost@gmail.com>
+;; Copyright © 2016–2025 Alex Kost <alezost@gmail.com>
 
 ;; This file is part of Guile-XOSD.
 
@@ -25,9 +25,221 @@
 
 (define-module (xosd bindings)
   #:use-module (ice-9 documentation)
-  #:use-module (xosd config))
+  #:use-module (rnrs bytevectors)
+  #:use-module (system foreign)
+  #:use-module (system foreign-library)
+  #:use-module (xosd config)
+  #:export (xosd-create
+            xosd-destroy
+            xosd-display-string
+            xosd-display-percentage
+            xosd-display-slider
+            xosd-hide
+            xosd-show
+            xosd-onscreen?
+            xosd-wait-until-no-display
+            xosd-set-bar-length!
+            xosd-set-align!
+            xosd-set-pos!
+            xosd-set-timeout!
+            xosd-set-font!
+            xosd-set-colour!
+            xosd-set-shadow-colour!
+            xosd-set-outline-colour!
+            xosd-set-vertical-offset!
+            xosd-set-horizontal-offset!
+            xosd-set-shadow-offset!
+            xosd-set-outline-offset!
+            xosd-get-colour
+            xosd-get-number-lines
+            xosd-scroll))
 
-(dynamic-call "init_xosd" (dynamic-link %library-file-name))
+
+;;; Auxiliary code
+
+;; XXX Maybe this should be separated into (xosd utils) module.
+
+(define-syntax-rule (print-error format-string args ...)
+  "Write some text and a newline to stderr using 'format'."
+  (begin
+    (format (current-error-port) format-string args ...)
+    (newline)))
+
+(define (integer->bool integer)
+  (not (= integer 0)))
+
+
+;; Enum definitions
+
+(define %XOSD_percentage 0)
+(define %XOSD_string 1)
+;; (define %XOSD_printf 2)      ; we don't use this feature
+(define %XOSD_slider 3)
+
+(define %XOSD_top 0)
+(define %XOSD_bottom 1)
+(define %XOSD_middle 2)
+
+(define %XOSD_left 0)
+(define %XOSD_center 1)
+(define %XOSD_right 2)
+
+
+;;; XOSD library and its procedures
+
+(define xosd-library
+  (let ((lib #f))
+    (lambda ()
+      "Return the linked 'libxosd' library."
+      (or lib
+          (begin (set! lib (load-foreign-library "libxosd"))
+                 lib)))))
+
+(define (xosd-procedure name)
+  "Helper function to get pointers to libxosd procedures."
+  (foreign-library-pointer (xosd-library) name))
+
+(define xosd-create
+  (let ((proc (xosd-procedure "xosd_create")))
+    (pointer->procedure '* proc (list int))))
+
+(define xosd-destroy
+  (let ((proc (xosd-procedure "xosd_destroy")))
+    (pointer->procedure int proc (list '*))))
+
+(define (xosd-display-string osd line-number string)
+  (let ((proc (xosd-procedure "xosd_display")))
+    ((pointer->procedure int proc (list '* int int '*))
+     osd line-number %XOSD_string (string->pointer string))))
+
+(define (xosd-display-percentage osd line-number percentage)
+  (let ((proc (xosd-procedure "xosd_display")))
+    ((pointer->procedure int proc (list '* int int int))
+     osd line-number %XOSD_percentage percentage)))
+
+(define (xosd-display-slider osd line-number percentage)
+  (let ((proc (xosd-procedure "xosd_display")))
+    ((pointer->procedure int proc (list '* int int int))
+     osd line-number %XOSD_slider percentage)))
+
+(define xosd-hide
+  (let ((proc (xosd-procedure "xosd_hide")))
+    (pointer->procedure int proc (list '*))))
+
+(define xosd-show
+  (let ((proc (xosd-procedure "xosd_show")))
+    (pointer->procedure int proc (list '*))))
+
+(define (xosd-onscreen? osd)
+  (let ((proc (xosd-procedure "xosd_is_onscreen")))
+    ;; TODO 'xosd_is_onscreen' can also return -1 (on failure).
+    (integer->bool ((pointer->procedure int proc (list '*))
+                    osd))))
+
+(define xosd-wait-until-no-display
+  (let ((proc (xosd-procedure "xosd_wait_until_no_display")))
+    (pointer->procedure int proc (list '*))))
+
+(define xosd-set-bar-length!
+  (let ((proc (xosd-procedure "xosd_set_bar_length")))
+    (pointer->procedure int proc (list '* int))))
+
+(define (xosd-set-pos! osd position)
+  (let ((proc (xosd-procedure "xosd_set_pos"))
+        (pos  (case position
+                ((top)    %XOSD_top)
+                ((middle) %XOSD_middle)
+                ((bottom) %XOSD_bottom)
+                (else (begin
+                        (print-error "Unknown position: ~a" position)
+                        #f)))))
+    (when pos
+      ((pointer->procedure int proc (list '* int))
+       osd pos))))
+
+(define (xosd-set-align! osd position)
+  (let ((proc (xosd-procedure "xosd_set_align"))
+        (pos  (case position
+                ((left)   %XOSD_left)
+                ((center) %XOSD_center)
+                ((right)  %XOSD_right)
+                (else (begin
+                        (print-error "Unknown align: ~a" position)
+                        #f)))))
+    (when pos
+      ((pointer->procedure int proc (list '* int))
+       osd pos))))
+
+(define xosd-set-timeout!
+  (let ((proc (xosd-procedure "xosd_set_timeout")))
+    (pointer->procedure int proc (list '* int))))
+
+(define (xosd-set-font! osd font)
+  (let ((proc (xosd-procedure "xosd_set_font")))
+    ((pointer->procedure int proc (list '* '*))
+     osd (string->pointer font))))
+
+(define (xosd-set-colour! osd colour)
+  (let ((proc (xosd-procedure "xosd_set_colour")))
+    ((pointer->procedure int proc (list '* '*))
+     osd (string->pointer colour))))
+
+(define (xosd-set-shadow-colour! osd colour)
+  (let ((proc (xosd-procedure "xosd_set_shadow_colour")))
+    ((pointer->procedure int proc (list '* '*))
+     osd (string->pointer colour))))
+
+(define (xosd-set-outline-colour! osd colour)
+  (let ((proc (xosd-procedure "xosd_set_outline_colour")))
+    ((pointer->procedure int proc (list '* '*))
+     osd (string->pointer colour))))
+
+(define xosd-set-vertical-offset!
+  (let ((proc (xosd-procedure "xosd_set_vertical_offset")))
+    (pointer->procedure int proc (list '* int))))
+
+(define xosd-set-horizontal-offset!
+  (let ((proc (xosd-procedure "xosd_set_horizontal_offset")))
+    (pointer->procedure int proc (list '* int))))
+
+(define xosd-set-shadow-offset!
+  (let ((proc (xosd-procedure "xosd_set_shadow_offset")))
+    (pointer->procedure int proc (list '* int))))
+
+(define xosd-set-outline-offset!
+  (let ((proc (xosd-procedure "xosd_set_outline_offset")))
+    (pointer->procedure int proc (list '* int))))
+
+(define (xosd-get-colour osd)
+  (let* ((endianness (native-endianness))
+         (int-size   (sizeof int))
+         (red-bv     (make-bytevector int-size))
+         (green-bv   (make-bytevector int-size))
+         (blue-bv    (make-bytevector int-size))
+         (red-ptr    (bytevector->pointer red-bv))
+         (green-ptr  (bytevector->pointer green-bv))
+         (blue-ptr   (bytevector->pointer blue-bv))
+         (proc       (xosd-procedure "xosd_get_colour"))
+         ;; Note: 'xosd_get_colour' returns X11 color formats which are
+         ;; 16-bit values (0-65535) instead of 8-bit values (0-255).
+         (result     ((pointer->procedure int proc (list '* '* '* '*))
+                      osd red-ptr green-ptr blue-ptr)))
+    (if (= result 0)
+        (list (bytevector-sint-ref red-bv 0   endianness int-size)
+              (bytevector-sint-ref green-bv 0 endianness int-size)
+              (bytevector-sint-ref blue-bv 0  endianness int-size))
+        #f)))
+
+(define xosd-get-number-lines
+  (let ((proc (xosd-procedure "xosd_get_number_lines")))
+    (pointer->procedure int proc (list '*))))
+
+(define xosd-scroll
+  (let ((proc (xosd-procedure "xosd_scroll")))
+    (pointer->procedure int proc (list '*))))
+
+
+;;; Documentation
 
 (unless (member %documentation-file-name documentation-files)
   (set! documentation-files (cons %documentation-file-name
